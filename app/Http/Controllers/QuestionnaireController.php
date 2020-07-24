df1be5bec008f0a9cca48013079a097bcc2d9eae
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-24 18:34:49 +0800

    initial commit

---
+++ app/Http/Controllers/QuestionnaireController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Questionnaire;
+
+class QuestionnaireController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+    public function create()
+    {
+    	return view('questionnaire.create');
+    }
+
+    public function store(Request $request)
+    {
+    	$validatedData = $request->validate([
+    		'title' => 'required',
+    		'purpose' => 'required'
+    	]);  	
+
+    	$questionnaire = new Questionnaire;
+    	$questionnaire->title = $request->title;
+    	$questionnaire->purpose = $request->purpose;
+    	$questionnaire->user_id = auth()->user()->id;
+    	$questionnaire->save();
+
+    	return redirect('/questionnaires/' .$questionnaire->id);
+
+    }
+
+    public function show($id)
+    {
+    	$questionnaire = Questionnaire::findOrFail($id);
+    	
+    	return view('questionnaire.show', compact('questionnaire'));
+    }
+}
