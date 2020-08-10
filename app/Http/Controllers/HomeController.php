fd7e2949f0b53ed4ff199ee2acad91a5ea69623c
Jaf <jaffreyrodriguez42gmail.com>
2020-08-10 18:04:19 +0800

    Add share ulr

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $questionnaires = auth()->user()->questionnaires;
+        // dd($questionnaires);
+        return view('home', compact('questionnaires'));
     }
 }
